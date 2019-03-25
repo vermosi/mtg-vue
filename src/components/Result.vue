fa753ab02a3a414bbbcf83403ae5b5dd087266d7
Kyle Ashmore <kyle.ashmore@hcahealthcare.com>
2019-03-24 22:44:37 -0500

    fixing view on mobile in master, bad kyle, don't do that.

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -10,27 +10,28 @@
 							<v-flex
 							v-for="card in cards"
 							:key="card.mtgo_id"
-							xs5
+							xs12 sm5
 							d-flex
 							>
 							<v-card flat tile class="d-flex">
 								<v-img v-for="card in cards" :key="card.mtgo_id" id="image" :src="card.image_uris.normal"></v-img>
 							</v-card>
 						</v-flex>
-						<v-flex v-for="card in cards" xs7>
+						<v-flex v-for="card in cards" sm7 xs12>
 							<v-card-title primary-title>
-								<div ref="myid">
-									<div class="headline">{{card.name}} {{card.mana_cost}}</div>
-									<p v-for="card in cards">{{card.oracle_text}}</p>
-									<p v-for="card in cards">{{card.power}} {{card.toughness}}</p>
-									<p>{{card.flavor_text}}</p>
-									<p>{{card.artist}}</p>
+								<div>
+									<h3 class="title">{{card.name}} {{card.mana_cost}}</h3>
+									<p class="description" v-for="card in cards">{{card.oracle_text}}</p>
+									<div v-for="card in cards">{{card.power}} / {{card.toughness}}</div>
+									<p class="flavor">{{card.flavor_text}}</p>
+									<p class="artist">{{card.artist}}</p>
 									<hr>
-									<div>
+									<div class="legalities">
 										<v-btn disabled>{{card.legalities.standard}} in Standard</v-btn>
 										<v-btn disabled>{{card.legalities.modern}} in Modern</v-btn>
 										<v-btn disabled>{{card.legalities.legacy}} in Legacy</v-btn>
 										<v-btn disabled>{{card.legalities.vintage}} in Vintage</v-btn>
+										<v-btn disabled>{{card.legalities.commander}} in Commander</v-btn>
 									</div>
 									<hr>
 								</div>
